b18f3797aa24e123ec4f37957b695ee84a673f67
Web-Developer-Hub <habibor144369@gmail.com>
2021-09-05 20:00:06 +0600

    ok programmer

--- js/bank.js
+++ js/bank.js
@@ -2,17 +2,22 @@
 document.getElementById('dipositButton').addEventListener('click', function () {
     const depositInput = document.getElementById('deposit_amount');
     const depositAmount = depositInput.value;
-    const depositNumber = document.getElementById('deposit_number');
-    const previousDipositAmount = depositNumber.innerText;
-    const totalDiposit = parseFloat(previousDipositAmount) + parseFloat(depositAmount);
-    depositNumber.innerText = totalDiposit;
+    if (depositAmount > 0) {
+        const depositNumber = document.getElementById('deposit_number');
+        const previousDipositAmount = depositNumber.innerText;
+        const totalDiposit = parseFloat(previousDipositAmount) + parseFloat(depositAmount);
+        depositNumber.innerText = totalDiposit;
 
-    // my blance code here..
-    const myBlance = document.getElementById('my_blance');
-    const myBlanceValue = myBlance.innerText;
-    totalMyBlance = parseFloat(myBlanceValue) + parseFloat(depositAmount);
-    myBlance.innerText = totalMyBlance;
-    depositInput.value = null;
+        // my blance code here..
+        const myBlance = document.getElementById('my_blance');
+        const myBlanceValue = myBlance.innerText;
+        totalMyBlance = parseFloat(myBlanceValue) + parseFloat(depositAmount);
+        myBlance.innerText = totalMyBlance;
+        depositInput.value = null;
+    }
+    else {
+        depositInput.value = null;
+    }
 })
 
 
@@ -20,16 +25,20 @@ document.getElementById('dipositButton').addEventListener('click', function () {
 document.getElementById('withdrawButton').addEventListener('click', function () {
     const withdrawAmount = document.getElementById('withdraw_amount');
     const withdrawAmountValue = withdrawAmount.value;
-    const withdrawBlance = document.getElementById('withdraw_blance');
-    const previousWithdrawAmount = withdrawBlance.innerText;
-    const totalWithdrawBlance = parseFloat(previousWithdrawAmount) + parseFloat(withdrawAmountValue);
-    withdrawBlance.innerText = totalWithdrawBlance;
-    
-    //my blance code here withdraw....
-    const myBlance = document.getElementById('my_blance');
-    const myBlanceValue = myBlance.innerText;
-    totalMyBlance = parseFloat(myBlanceValue) - parseFloat(withdrawAmountValue);
-    myBlance.innerText = totalMyBlance;
-
-    withdrawAmount.value = null;
+    if (withdrawAmountValue > 0) {
+        const withdrawBlance = document.getElementById('withdraw_blance');
+        const previousWithdrawAmount = withdrawBlance.innerText;
+        const totalWithdrawBlance = parseFloat(previousWithdrawAmount) + parseFloat(withdrawAmountValue);
+        withdrawBlance.innerText = totalWithdrawBlance;
+        
+        //my blance code here withdraw....
+        const myBlance = document.getElementById('my_blance');
+        const myBlanceValue = myBlance.innerText;
+        totalMyBlance = parseFloat(myBlanceValue) - parseFloat(withdrawAmountValue);
+        myBlance.innerText = totalMyBlance;
+        withdrawAmount.value = null;
+    }
+    else {
+        withdrawAmount.value = null;
+    }
 })
